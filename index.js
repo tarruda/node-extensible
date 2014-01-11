da8f302f5d66662f543af851174111f1f2721594
Thiago de Arruda <tpadilha84@gmail.com>
2014-01-10 23:40:04 -0300

    Refactor to provide better runtime metadata about
    layers

--- index.js
+++ index.js
@@ -2,15 +2,22 @@
 var xtend = require('xtend');
 
 
+var create = Object.create;
+
+
 function installLayerClass(target, sup) {
-  target._layerClass = function Layer(layer, next) {
-    this._obj = target;
-    this._layer = layer;
+  target._layerClass = function Layer(impl, next) {
+    this.impl = impl;
     this.next = next;
+    this.data = {
+      layer: this,
+      next: next,
+      obj: target
+    };
   };
 
   if (sup) {
-    target._layerClass.prototype = Object.create(sup._layerClass.prototype, {
+    target._layerClass.prototype = create(sup._layerClass.prototype, {
       constructor: {
         value: target._layerClass,
         enumerable: false,
@@ -25,77 +32,105 @@ function installLayerClass(target, sup) {
 // Base object only containing the basic infrastructure for adding
 // methods and layers 
 function Extensible() {
-  var _this = this;
-
   // methods installed into this object
-  this.methods = [];
-  // first and last extensions
-  this.top = null;
+  this._methods = [];
+  this._methodsByName = {};
+  this._top = null;
   installLayerClass(this);
 }
 
 
 // Wraps the object into a new layer
-Extensible.prototype.layer = function(layer, opts) {
+Extensible.prototype.use = function(layer, opts) {
   if (typeof layer === 'function')
     // call factory function
     layer = layer.call(this, opts);
 
-  this.top = new this._layerClass(layer, this.top);
+  this._top = new this._layerClass(layer, this._top);
 };
 
 
 // Adds a new extensible method to the object.
-Extensible.prototype.method = function(name, args, metadata) {
+Extensible.prototype.addMethod = function(name, args, metadata) {
   if (name in this)
     throw new Error(
       "Name '" + name + "' already exists in the prototype chain");
 
-  var nameStr = JSON.stringify(name);
+  var str = JSON.stringify(name);
 
   this[name] =
     new Function(args,
-      '  \nreturn this.top[' + nameStr + '](' + args + ');\n');
+      '\n  var next = this._top;' +
+      '\n  return next[' + str + '].call(this, ' + args + ', next);\n');
 
   this._layerClass.prototype[name] =
     new Function(args,
-      '\n  var _this = this;\n' +
-      '  if (this._layer[' + nameStr + '])\n' +
-      '    return this._layer[' + nameStr + '].call(this._obj, ' + args + ', \n' +
-      '      function(' + args + ') {\n' +
-      '        _this.next[' + nameStr + '](' + args + ');\n' +
-      '      });\n' +
-      '  return this.next[' + nameStr + '](' + args + ');\n');
+      '\n  var _this = this;' +
+      '\n  var layer = arguments[arguments.length - 1];' +
+      '\n  var next = layer.next;' +
+      '\n  if (layer.impl[' + str + '])' +
+      '\n    return layer.impl[' + str + '].call(this, ' + args + ', ' +
+      '\n      function(' + args + ') {' +
+      '\n        next[' + str + '].call(_this, ' + args + ', next);' +
+      '\n      }, layer);' +
+      '\n  return next[' + str + '].call(this, ' + args + ', next);\n');
 
-  this.methods.push(xtend({
+  metadata = xtend({
     name: name,
     args: args && args.split(/\s*,\s*/) || []
-  }, metadata));
+  }, metadata);
+
+  this._methods.push(metadata);
+  this._methodsByName[name] = metadata;
+};
+
+
+// Returns metadata associated with the method `name`.
+Extensible.prototype.getMethod = function(name) {
+  return this._methodsByName[name];
+};
+
+
+// Iterates through each installed method
+Extensible.prototype.eachMethod = function(cb) {
+  for (var i = 0, l = this._methods.length; i < l; i++)
+    cb(this._methods[i]);
+};
+
+
+// Iterates through each layer of this object
+Extensible.prototype.eachLayer = function(cb) {
+  function next(layer) {
+    if (!layer) return;
+    if (layer.next) next(layer.next);
+    cb(layer);
+  }
+  next(this._top);
+};
+
+
+// Creates a new object whose prototype is set to the current object.
+Extensible.prototype.instance = function() {
+  return create(this);
 };
 
 
 // Forks by creating a new object with all methods and layers from the current
 // object.
 //
-// Only the method descriptors and layers are copied, as the new
-// object's prototype is set to the current object, which will result in
-// all methods being inherited.
+// The difference from 'instance' is that the new object receives copies
+// of the layers and method descriptors, so it can be modified independently
+// from the current object.
 //
 // The new object will also have its own dedicated layer class, which inherits
 // from the current layer class.
 Extensible.prototype.fork = function() {
-  var rv = Object.create(this);
+  var rv = this.instance();
 
-  rv.top = null;
-  rv.methods = this.methods.slice();
-
-  function copyLayer(layer) {
-    if (!layer) return;
-    if (layer.next) copyLayer(layer.next);
-    rv.layer(layer._layer);
-  }
-  copyLayer(this.top);
-
+  rv._top = null;
+  rv._methods = this._methods.slice();
+  rv._methodsByName = xtend({}, this._methodsByName);
+  this.eachLayer(function(layer) { rv.use(layer.impl); });
   installLayerClass(rv, this);
 
   return rv;
--- test/index.js
+++ test/index.js
@@ -12,43 +12,46 @@ describe('extensible object', function() {
 
   beforeEach(function() {
     top = {};
-    top[methodName] = function(arg1, arg2, arg3, cb, next) {
+    top[methodName] = function(arg1, arg2, arg3, cb, next, layer) {
       equal(this, obj);
+      equal(top, layer.impl);
       next(arg1 * 64, arg2, arg3, function(err, rv) { cb(err, rv / 64); });
     };
     mid = {};
-    mid[methodName] = function(arg1, arg2, arg3, cb, next) {
+    mid[methodName] = function(arg1, arg2, arg3, cb, next, layer) {
       equal(this, obj);
+      equal(mid, layer.impl);
       next(arg1 * 64, arg2, arg3, function(err, rv) { cb(err, rv / 64); });
     };
     bot = function(opts) {
       equal(this, obj);
       // add a method
-      this.method(opts.methodName, 'arg1, arg2, arg3, cb');
+      this.addMethod(opts.methodName, 'arg1, arg2, arg3, cb');
       var rv = {};
-      rv[methodName] = function(arg1, arg2, arg3, cb, next) {
+      rv[methodName] = function(arg1, arg2, arg3, cb, next, layer) {
         equal(this, obj);
+        equal(rv, layer.impl);
         cb([1, 2], arg1);
       };
       return rv;
     };
 
     obj = extensible();
-    obj.layer(bot, {methodName: methodName});
-    obj.layer(mid);
-    obj.layer(top);
+    obj.use(bot, {methodName: methodName});
+    obj.use(mid);
+    obj.use(top);
 
-    sinon.spy(obj.top._layer, methodName);
-    sinon.spy(obj.top.next._layer, methodName);
-    sinon.spy(obj.top.next.next._layer, methodName);
+    sinon.spy(obj._top.impl, methodName);
+    sinon.spy(obj._top.next.impl, methodName);
+    sinon.spy(obj._top.next.next.impl, methodName);
   });
 
 
   it('passes arguments from top to bottom layer', function() {
     obj[methodName](1, 3, 4, function() {});
-    assert(obj.top._layer[methodName].calledWith(1, 3, 4));
-    assert(obj.top.next._layer[methodName].calledWith(64, 3, 4));
-    assert(obj.top.next.next._layer[methodName].calledWith(4096, 3, 4));
+    assert(obj._top.impl[methodName].calledWith(1, 3, 4));
+    assert(obj._top.next.impl[methodName].calledWith(64, 3, 4));
+    assert(obj._top.next.next.impl[methodName].calledWith(4096, 3, 4));
   });
 
 
@@ -61,6 +64,39 @@ describe('extensible object', function() {
   });
 
 
+  describe('eachLayer', function() {
+    it('iterates through each layer', function() {
+      var items = [];
+      obj.eachLayer(function(layer) { items.push(layer.impl); });
+      deepEqual([obj._top.next.next.impl, mid, top], items);
+    });
+  });
+
+
+  describe('eachMethod', function() {
+    it('iterates through each method metadata', function() {
+      var items = [];
+      obj.eachMethod(function(method) { items.push(method); });
+      deepEqual([{name: 'm', args: ['arg1', 'arg2', 'arg3', 'cb']}], items);
+    });
+  });
+
+
+  describe('getMethod', function() {
+    it('gets method by name', function() {
+      deepEqual({name: 'm', args: ['arg1', 'arg2', 'arg3', 'cb']},
+                obj.getMethod('m'));
+    });
+  });
+
+
+  describe('instance', function() {
+    it('links through the prototype chain', function() {
+      assert(obj.isPrototypeOf(obj.instance()));
+    });
+  });
+
+
   describe('fork', function() {
     var forked;
     beforeEach(function() {
@@ -68,42 +104,47 @@ describe('extensible object', function() {
     });
 
 
+    it('links through the prototype chain', function() {
+      assert(obj.isPrototypeOf(forked));
+    });
+
+
     it('should copy method descriptors', function() {
-      notEqual(obj.methods, forked.methods);
+      notEqual(obj._methods, forked._methods);
       deepEqual([{name: 'm', args: ['arg1', 'arg2', 'arg3', 'cb']}],
-                obj.methods);
+                obj._methods);
       deepEqual([{name: 'm', args: ['arg1', 'arg2', 'arg3', 'cb']}],
-                forked.methods);
+                forked._methods);
     });
 
 
     it('should copy layers', function() {
-      notEqual(obj.top, forked.top);
-      notEqual(obj.top.next, forked.top.next);
-      notEqual(obj.top.next.next, forked.top.next.next);
-      equal(obj.top._layer, forked.top._layer);
-      equal(obj.top.next._layer, forked.top.next._layer);
-      equal(obj.top.next.next._layer, forked.top.next.next._layer);
+      notEqual(obj._top, forked._top);
+      notEqual(obj._top.next, forked._top.next);
+      notEqual(obj._top.next.next, forked._top.next.next);
+      equal(obj._top.impl, forked._top.impl);
+      equal(obj._top.next.impl, forked._top.next.impl);
+      equal(obj._top.next.next.impl, forked._top.next.next.impl);
     });
 
 
     it('should copy layers', function() {
-      notEqual(obj.top, forked.top);
-      notEqual(obj.top.next, forked.top.next);
-      notEqual(obj.top.next.next, forked.top.next.next);
-      equal(obj.top._layer, forked.top._layer);
-      equal(obj.top.next._layer, forked.top.next._layer);
-      equal(obj.top.next.next._layer, forked.top.next.next._layer);
+      notEqual(obj._top, forked._top);
+      notEqual(obj._top.next, forked._top.next);
+      notEqual(obj._top.next.next, forked._top.next.next);
+      equal(obj._top.impl, forked._top.impl);
+      equal(obj._top.next.impl, forked._top.next.impl);
+      equal(obj._top.next.next.impl, forked._top.next.next.impl);
     });
 
 
     describe('forked object', function() {
       it("wont affect the original object methods", function() {
-        forked.method('y');
+        forked.addMethod('y');
         deepEqual([{name: 'm', args: ['arg1', 'arg2', 'arg3', 'cb']}],
-                  obj.methods);
+                  obj._methods);
         deepEqual([{name: 'm', args: ['arg1', 'arg2', 'arg3', 'cb']},
-          {name: 'y', args: []}], forked.methods);
+          {name: 'y', args: []}], forked._methods);
         equal(true, 'm' in obj);
         equal(false, 'y' in obj);
         equal(true, 'y' in forked);
@@ -111,12 +152,12 @@ describe('extensible object', function() {
 
 
       it("wont affect the original object layers", function() {
-        forked.layer(top);
-        equal(top, forked.top._layer);
-        equal(top, forked.top.next._layer);
-        equal(mid, forked.top.next.next._layer);
-        equal(top, obj.top._layer);
-        equal(mid, obj.top.next._layer);
+        forked.use(top);
+        equal(top, forked._top.impl);
+        equal(top, forked._top.next.impl);
+        equal(mid, forked._top.next.next.impl);
+        equal(top, obj._top.impl);
+        equal(mid, obj._top.next.impl);
       });
 
 
